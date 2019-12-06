36850e3559f97d9131dfe648ec72f08bd58778cf
Nicole Dow <nikki.dow71@gmail.com>
2019-12-06 11:06:56 -0700

    add header and styling

--- mandala-maker/src/Components/Navbar.js
+++ mandala-maker/src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar">
       <button className="navbar-brand">Mandalas</button>
       <button className="navbar-brand">Get Inspired</button>
       <button className="navbar-brand">New</button>
