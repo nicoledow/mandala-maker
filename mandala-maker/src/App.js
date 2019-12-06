81e58198f39bc1fd7dddda8342e74285caf2ace9
Nicole Dow <nikki.dow71@gmail.com>
2019-12-05 17:20:04 -0700

    add dummy navbar

--- mandala-maker/src/App.js
+++ mandala-maker/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Canvas from './Containers/Canvas';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Canvas />
     </div>
   );
---
+++ mandala-maker/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <button className="navbar-brand">Mandalas</button>
+      <button className="navbar-brand">Get Inspired</button>
+      <button className="navbar-brand">New</button>
+    </nav>
+  )
+}
+
+export default Navbar;
