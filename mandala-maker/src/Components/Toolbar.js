fe8b5431cc5528474ee5f3f6b42241dab22aa488
Nicole Dow <nikki.dow71@gmail.com>
2019-12-06 11:24:05 -0700

    render ellipse button that changes color when clicked

--- mandala-maker/src/Components/Toolbar.js
+++ mandala-maker/src/Components/Toolbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import EllipseButton from './EllipseButton';
 
 export default class Toolbar extends React.Component {
+
     render() {
         return(
             <nav>
                 <button><i class="fas fa-undo"></i></button>
-                <button><i class="far fa-circle"></i></button>
+                <EllipseButton />
                 <button><i class="far fa-square"></i></button>
                 <button><i class="fas fa-caret-up"></i></button>
             </nav>
