2e8f01e44b8dacefe29995001de2f23ed2da64e1
Nicole Dow <nikki.dow71@gmail.com>
2019-12-07 11:37:32 -0700

    render colorpalette comp

--- mandala-maker/src/App.js
+++ mandala-maker/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import ShapesContainer from './Containers/ShapesContainer';
 import Navbar from './Components/Navbar';
+import ColorPalette from './Components/ColorPalette';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ColorPalette />
       <ShapesContainer />
     </div>
   );
