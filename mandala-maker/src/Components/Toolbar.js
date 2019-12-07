16614c1a51ecb938dfc3f7acfd119d978b11ae5f
Nicole Dow <nikki.dow71@gmail.com>
2019-12-06 17:41:15 -0700

    add currentlyDrawingShape to shapesContainer state

--- mandala-maker/src/Components/Toolbar.js
+++ mandala-maker/src/Components/Toolbar.js
@@ -6,9 +6,9 @@ export default class Toolbar extends React.Component {
         return(
             <nav>
                 <button id="undo-btn"><i class="fas fa-undo"></i></button>
-                <button id="ellipse-btn"><i class="far fa-circle"></i></button>
-                <button id="square-btn"><i class="far fa-square"></i></button>
-                <button id="triangle-btn"><i class="fas fa-caret-up"></i></button>
+                <button id="ellipse" onClick={e => this.props.chooseShape(e)}><i class="far fa-circle"></i></button>
+                <button id="square"><i class="far fa-square"></i></button>
+                <button id="triangle"><i class="fas fa-caret-up"></i></button>
             </nav>
         )
     }
--- mandala-maker/src/Containers/ShapesContainer.js
+++ mandala-maker/src/Containers/ShapesContainer.js
@@ -4,16 +4,24 @@ import Canvas from './Canvas';
 import { connect } from 'react-redux';
 
 class ShapesContainer extends React.Component {
+    state = {
+        currentDrawingShape: ''
+    }
 
 
     chooseShape = e => {
-        console.log(e)
+        this.setState({ currentDrawingShape: e.target.parentElement.id })
+    }
+
+    drawEllipse = e => {
+        console.log('drawing ellipse');
     }
 
     render(){
+        console.log(this.state)
         return(
             <div>
-                <Canvas />
+                <Canvas currentDrawingShape={this.state.currentDrawingShape} />
                 <Toolbar chooseShape={this.chooseShape}/>
             </div>
         )
