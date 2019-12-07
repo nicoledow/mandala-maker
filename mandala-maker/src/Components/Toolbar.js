46ee8239d862a9062ea18607979aec850516414f
Nicole Dow <nikki.dow71@gmail.com>
2019-12-07 16:23:51 -0700

    new triangle icon

--- mandala-maker/src/Components/Toolbar.js
+++ mandala-maker/src/Components/Toolbar.js
@@ -8,7 +8,7 @@ export default class Toolbar extends React.Component {
                 <button id="undo-btn"><i class="fas fa-undo"></i></button>
                 <button id="ellipse" onClick={e => this.props.chooseShape(e)}><i class="far fa-circle"></i></button>
                 <button id="square" onClick={e => this.props.chooseShape(e)}><i class="far fa-square"></i></button>
-                <button id="triangle" onClick={e => this.props.chooseShape(e)}><i class="fas fa-caret-up"></i></button>
+                <button id="triangle" onClick={e => this.props.chooseShape(e)}>▲</button>
             </nav>
         )
     }
