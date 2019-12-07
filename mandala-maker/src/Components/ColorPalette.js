ca5057ed7e021012bb1cf0d8a4f5cca95c4abeba
Nicole Dow <nikki.dow71@gmail.com>
2019-12-07 12:09:45 -0700

    add all color btns

--- mandala-maker/src/Components/ColorPalette.js
+++ mandala-maker/src/Components/ColorPalette.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default class ColorPalette extends React.Component {
+class ColorPalette extends React.Component {
     
     render() {
       return (
           <div id="color-palette">
-              ColorPalette
+              <h4>Pick a color:</h4>
+              <div className="navy-box">Navy Blue</div>
+              <div className="blue-box">Blue</div>
+              <div className="aqua-box">Aqua</div>
+              <div className="olive-box">Olive</div>
+              <div className="green-box">Green</div>
+              <div className="lime-box">Lime</div>
+              <div className="yellow-box">Yellow</div>
+              <div className="orange-box">Orange</div>
+              <div className="purple-box">Purple</div>
+              <div className="fuschia-box">Fuschia</div>
+              <div className="red-box">Red</div>
+              <div className="maroon-box">Maroon</div>
+              <div className="gray-box">Gray</div>
+              <div className="black-box">Black</div>
           </div>
       )
     }
 }
+
+const mapDispatchToProps = dispatch => {
+  return { 
+      changeSelectedColor: color => dispatch({ type: 'CHANGE_SELECTED_COLOR', color})
+  }
+}
+
+export default connect(null, mapDispatchToProps)(ColorPalette);
--- mandala-maker/src/reducers/handledrawings.js
+++ mandala-maker/src/reducers/handledrawings.js
@@ -1,7 +1,9 @@
-const handleDrawings = (state = { shapes: []}, action) => {
+const handleDrawings = (state = { shapes: [], selectedColor: '' }, action) => {
     switch(action.type) {
         case 'ADD_SHAPE':
           return { ...state, shapes: [...state.shapes, action.shape]}
+        case 'CHANGE_SELECTED_COLOR':
+          console.log('going to change selected color');
         default:
             return state;
     }
