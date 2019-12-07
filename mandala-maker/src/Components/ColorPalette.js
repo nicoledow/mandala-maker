7f8cc7b9a8978d1d6a95763b714e306ff9ba91c4
Nicole Dow <nikki.dow71@gmail.com>
2019-12-07 16:01:42 -0700

    add # to each button id

--- mandala-maker/src/Components/ColorPalette.js
+++ mandala-maker/src/Components/ColorPalette.js
@@ -11,20 +11,20 @@ class ColorPalette extends React.Component {
       return (
           <div id="color-palette">
               <h4>Pick a color:</h4>
-              <div className="navy-box" id="001f3f" onClick={e => this.pickColor(e)}>Navy Blue</div>
-              <div className="blue-box" id="0074d9" onClick={e => this.pickColor(e)}>Blue</div>
-              <div className="aqua-box" id="7fdbff" onClick={e => this.pickColor(e)}>Aqua</div>
-              <div className="olive-box" id="3d9970" onClick={e => this.pickColor(e)}>Olive</div>
-              <div className="green-box" id="2ecc40" onClick={e => this.pickColor(e)}>Green</div>
-              <div className="lime-box" id="01ff70" onClick={e => this.pickColor(e)}>Lime</div>
-              <div className="yellow-box" id="ffdc00" onClick={e => this.pickColor(e)}>Yellow</div>
-              <div className="orange-box" id="ff8515b" onClick={e => this.pickColor(e)}>Orange</div>
-              <div className="purple-box" id="b10dc9" onClick={e => this.pickColor(e)}>Purple</div>
-              <div className="fuschia-box" id="f012be" onClick={e => this.pickColor(e)}>Fuschia</div>
-              <div className="red-box" id="ff4136" onClick={e => this.pickColor(e)}>Red</div>
-              <div className="maroon-box" id="85144b" onClick={e => this.pickColor(e)}>Maroon</div>
-              <div className="gray-box" id="aaaaaa" onClick={e => this.pickColor(e)}>Gray</div>
-              <div className="black-box" id="111111" onClick={e => this.pickColor(e)}>Black</div>
+              <button className="navy-box" id="#001f3f" onClick={e => this.pickColor(e)}>Navy Blue</button>
+              <button className="blue-box" id="0074d9" onClick={e => this.pickColor(e)}>Blue</button>
+              <button className="aqua-box" id="#7fdbff" onClick={e => this.pickColor(e)}>Aqua</button>
+              <button className="olive-box" id="#3d9970" onClick={e => this.pickColor(e)}>Olive</button>
+              <button className="green-box" id="#2ecc40" onClick={e => this.pickColor(e)}>Green</button>
+              <button className="lime-box" id="#01ff70" onClick={e => this.pickColor(e)}>Lime</button>
+              <button className="yellow-box" id="#ffdc00" onClick={e => this.pickColor(e)}>Yellow</button>
+              <button className="orange-box" id="#ff8515b" onClick={e => this.pickColor(e)}>Orange</button>
+              <button className="purple-box" id="#b10dc9" onClick={e => this.pickColor(e)}>Purple</button>
+              <button className="fuschia-box" id="#f012be" onClick={e => this.pickColor(e)}>Fuschia</button>
+              <button className="red-box" id="#ff4136" onClick={e => this.pickColor(e)}>Red</button>
+              <button className="maroon-box" id="#85144b" onClick={e => this.pickColor(e)}>Maroon</button>
+              <button className="gray-box" id="#aaaaaa" onClick={e => this.pickColor(e)}>Gray</button>
+              <button className="black-box" id="#111111" onClick={e => this.pickColor(e)}>Black</button>
           </div>
       )
     }
