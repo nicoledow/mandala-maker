8d03ae833bae9712dec176f63357cc23e596ae56
Nicole Dow <nikki.dow71@gmail.com>
2019-12-08 13:58:07 -0700

    fix export statement

--- mandala-maker/src/Containers/Canvas.js
+++ mandala-maker/src/Containers/Canvas.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export default const Canvas = props => {
+const Canvas = props => {
     return (
       <canvas ref="canvas" height={window.innerHeight * 0.8} width={window.innerWidth * 0.8} />
     )
 }
+
+export default Canvas;
