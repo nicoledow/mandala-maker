d3e3d5c40ad79f8e49ff07627c98a5240e1de2bf
Nicole Dow <nikki.dow71@gmail.com>
2019-12-08 13:58:54 -0700

    remove comments

--- mandala-maker/src/reducers/handledrawings.js
+++ mandala-maker/src/reducers/handledrawings.js
@@ -1,7 +1,6 @@
 const handleDrawings = (state = { shapes: [], selectedColor: '', currentShape: '' }, action) => {
     switch(action.type) {
         case 'ADD_CIRCLE':
-          //return { ...state, shapes: {...state.shapes, circles: [...state.shapes.circles, action.circle]}}
           return {...state, shapes: [...state.shapes, action.circle]}
         case 'CHANGE_SELECTED_COLOR':
           return {...state, selectedColor: action.color }
