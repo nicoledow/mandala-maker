26f9053c5676ac87872dd5464a6f4440248e4baf
Nicole Dow <nikki.dow71@gmail.com>
2019-12-09 10:52:51 -0700

    new createEllipse fn lets user control radius

--- mandala-maker/src/Containers/Canvas.js
+++ mandala-maker/src/Containers/Canvas.js
@@ -28,17 +28,48 @@ class Canvas extends React.Component {
 
     switch(currentShape){
         case 'ellipse':
-            canvas.addEventListener('mousedown', this.drawEllipse);
+            //canvas.addEventListener('mousedown', this.drawEllipse);
+            canvas.addEventListener('mousedown', e => {
+              let {x, y} = this.getMousePosition(canvas, e);
+              this.setState({ startX: x, startY: y });
+              this.beginEllipse();
+            });
             break;
         default:
             return;
     }
   }
 
+  beginEllipse = () => {
+    const canvas = document.getElementById('canvas');
+    document.addEventListener('mouseup', e => {
+      let {x, y} = this.getMousePosition(canvas, e);
+      this.setState({ endX: x, endY: y });
+      this.createEllipse();
+    })
+  }
+
+  createEllipse = () => {
+    console.log('in createEllipse fn');
+    const canvas = document.getElementById('canvas');
+    let midpointX = (this.state.startX + this.state.endX) / 2;
+    let midpointY = (this.state.startY + this.state.endY) / 2;
+    let ctx = this.state.ctx;
+    let radius = this.getRadius(this.state.startX, this.state.startY, this.state.endX, this.state.endY) 
+
+    ctx.fillStyle = 'pink';
+    ctx.beginPath();
+    ctx.arc(midpointX, midpointY, radius, 0, Math.PI * 2.0);
+    ctx.fill();
+    console.log('startX', this.state.startX)
+    console.log('startY', this.state.startY)
+    console.log('midpointX', midpointX)
+    console.log('midpointY', midpointY)
+    this.props.addEllipse({ shapeType: 'ellipse', x: midpointX, y: midpointY, radius})
+  }
   
   drawEllipse = e => {
     const canvas = document.getElementById('canvas');
-    console.log('going to draw ellipse')
     canvas.addEventListener('click', (e) => {
         console.log(this.props.selectedColor)
         let {x, y} = this.getMousePosition(canvas, e);
@@ -47,7 +78,7 @@ class Canvas extends React.Component {
         ctx.beginPath();
         ctx.arc(x, y, 10, 0, Math.PI * 2);
         ctx.fill();
-        this.props.addEllipse({ shapeType: 'ellipse', x, y, radius: 10})
+        //this.props.addEllipse({ shapeType: 'ellipse', x, y, radius: 10})
     })
   }
 
@@ -57,6 +88,10 @@ class Canvas extends React.Component {
       x: evt.clientX - rect.left,
       y: evt.clientY - rect.top
     };
+  }
+
+  getRadius = (startX, startY, endX, endY) => {
+    return Math.hypot(endX - startX, endY - startY)
   }
 
   render() {
