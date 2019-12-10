56bb6e51c9db98bd6e68f12b28bd59e0d830e994
Nicole Dow <nikki.dow71@gmail.com>
2019-12-09 17:34:56 -0700

    working on getCurvePoints fn

--- mandala-maker/src/Containers/Canvas.js
+++ mandala-maker/src/Containers/Canvas.js
@@ -16,6 +16,11 @@ class Canvas extends React.Component {
     this.drawCenterCircle();    
   }
 
+  //if state such as currentShape or selectedColor is updated (via toolbar or color palette buttons), drawingMode is verified/changed
+  componentDidUpdate() {
+    this.selectDrawingMode();
+  }
+
   //returns coordinates of canvas center as [x, y]
   getCenter = canvas => {
     return [(canvas.width / 2), (canvas.height / 2)]
@@ -40,6 +45,29 @@ class Canvas extends React.Component {
       x: evt.clientX - rect.left,
       y: evt.clientY - rect.top
     };
+  }
+
+  selectDrawingMode = () => {
+    switch(this.props.currentShape) {
+      case 'curve':
+        this.getCurvePoints();
+      default:
+        return;
+    }
+  }
+
+  //adds event listener to canvas to await points for drawing a quadratic curve
+  getCurvePoints = () => {
+    //canvas listens for clicks, stores event in an array
+    //canvas listens for second click, stores that event in an array
+    //pass this array into a callback fn that sets state with 2 clicks' data
+    const clicks = [];
+    while (clicks.length <= 1) {
+      document.getElementById('canvas').addEventListener('click', e => {
+        clicks.push(e);
+      })
+    }
+    console.log(clicks);
   }
 
   render() {
